46f93a7a3b4150f015f1dbcc724916bd74d25369
kaleabm18 <kaleabm18@gmail.com>
2021-09-14 02:34:25 -0700

    "view customer info"

---
+++ customers.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <title>View Customers</title>
+</head>
+<body>
+  <div class="container mt-4">
+   <h2>Customers</h2>
+   <table class="tabel tabel-striped">
+       <thead>
+       <tr>
+           <th>Customer Id</th>
+           <th>Name</th>
+           <th>Email</th>
+           <th>Date</th>
+           <th></th>
+       </tr>
+       </thead>
+   </table>
+  </div>
+</body>
+</html>
