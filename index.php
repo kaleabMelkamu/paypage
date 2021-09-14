cbf8ae0b95d3d6f7bc81da4d4183a0cd130cbe06
kaleabm18 <kaleabm18@gmail.com>
2021-09-13 23:01:23 -0700

    'add css'

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
-    </head>
+</head>
     <body>
     
+    <form action="/charge" method="post" id="payment-form">
+  <div class="form-row">
+    <label for="card-element">
+      Credit or debit card
+    </label>
+    <div id="card-element">
+      <!-- A Stripe Element will be inserted here. -->
+    </div>
+
+    <!-- Used to display Element errors. -->
+    <div id="card-errors" role="alert"></div>
+  </div>
+
+  <button>Submit Payment</button>
+</form>
+<script src="https://js.stripe.com/v3/"></script>
     </body>
 </html>>
